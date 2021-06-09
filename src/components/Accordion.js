0c8b9ed1f25ee6ec8ca03bae04fca51d2240393d
Jmiller1294 <jayman1294@gmail.com>
2021-06-08 23:52:40 -0400

    created search component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,22 +3,22 @@ import React, { useState } from 'react';
 const Accordion = ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(null);
   
-  
-  
   const onTitleClick = (index) => {
     setActiveIndex(index);
   }
   
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+
     return <React.Fragment key={item.title}>
       <div 
-        className="title active"
+        className={`title ${active}`}
         onClick={() => onTitleClick(index)}
       >
         <i className="dropdown icon"></i>
         {item.title}
       </div>
-      <div className="content active">
+      <div className={`content ${active}`}>
         <p>{item.content}</p>
       </div>
     </React.Fragment>
@@ -27,7 +27,6 @@ const Accordion = ({ items }) => {
   return (
     <div className="ui styled accordion">
       {renderedItems}
-      <h1>{activeIndex}</h1>
     </div>
   )
 }
