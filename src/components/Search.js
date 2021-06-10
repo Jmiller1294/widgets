ea780ea097aa7cb7c0e02ebb56e2c4fce792d55d
Justin Miller <jayman1294@gmail.com>
2021-06-10 14:33:28 -0400

    mapped through results from wikioedia api and displayed on page

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,11 +3,14 @@ import axios from 'axios';
 import { waitForElementToBeRemoved } from '@testing-library/dom';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
+
+  console.log(results);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get('https://en.wikipedia.org/w/api/php', {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: { 
           action: 'query',
           list: 'search',
@@ -16,9 +19,24 @@ const Search = () => {
           srsearch: term
         },
       });
+      setResults(data.query.search)
     };
     search();
   }, [term])
+
+  const renderedResults = results.map((result) => {
+    return(
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          {result.snippet}
+        </div>
+
+      </div>
+    )
+  })
 
   return (
     <div>
@@ -27,6 +45,9 @@ const Search = () => {
           <label>Enter Search Term</label>
           <input value={term}  onChange={e => setTerm(e.target.value)} className="input" />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   )
