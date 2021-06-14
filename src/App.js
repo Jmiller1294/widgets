75de336c01891ccbd6b0c4fb0badf64297b384c8
Justin Miller <jayman1294@gmail.com>
2021-06-13 22:47:08 -0400

    created dropdown component and options array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 
 const items = [
@@ -19,7 +20,7 @@ const items = [
 
 const App = () => {
   return (
-    <Search />
+    <Dropdown />
   )
 }
 export default App;
